5567c050c142b9fc5ed60c23051ecebb6cd605d6
Gyeongho Park <59360980+kayheypark@users.noreply.github.com>
2025-06-02 02:40:24 +0900

    feat: CORS (개발환경, 서비스환경)

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,21 @@ import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // CORS 설정
+  app.enableCors({
+    origin: [
+      "http://localhost:3000",
+      "https://localhost:3000",
+      "http://www.crefans.com",
+      "https://www.crefans.com",
+      "http://crefans.com",
+      "https://crefans.com",
+    ],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
